194e1e37013b75ab0acdcb3137d048941381fa18
Kanchan Verma <kanchan0106verma@gmail.com>
2026-06-04 21:54:07 +0530

    Added PDF resume parsing

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
+from pypdf import PdfReader
 
 st.set_page_config(page_title="CareerPilot AI", page_icon="🚀")
 
 st.title("🚀 CareerPilot AI")
 st.subheader("AI Interview Preparation Platform")
-
-st.write("Upload your resume and prepare for interviews with AI.")
 
 uploaded_file = st.file_uploader(
     "Upload your Resume (PDF)",
@@ -13,4 +12,21 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file:
+    pdf = PdfReader(uploaded_file)
+
+    resume_text = ""
+
+    for page in pdf.pages:
+        text = page.extract_text()
+        if text:
+            resume_text += text
+
     st.success("Resume uploaded successfully!")
+
+    st.subheader("Resume Content")
+
+    st.text_area(
+        "Extracted Resume Text",
+        resume_text,
+        height=300
+    )
